50d20401b398ff16740b7fb6f0e3633f2f62445a
VishaalVinny <151037301+VishaalVinny@users.noreply.github.com>
2023-11-20 19:58:41 +0530

    CarCare Vehicle Repairing and Maintenance Company.
    Suppliers' Management Form

---
+++ CarCareSupplier/CarCareSupplier/src/main/java/Control/SupplierController.java
@@ -0,0 +1,42 @@
+package Control;
+
+import Model.Supplier;
+
+import javax.swing.*;
+import java.util.ArrayList;
+public class SupplierController {
+    private ArrayList<Supplier> suppliers;
+    Supplier supplier;
+    public SupplierController(ArrayList<Supplier> suppliers) {
+        this.suppliers = suppliers;
+    }
+    public void addSupplier(Supplier newSupplier) {
+        suppliers.add(newSupplier);
+    }
+    public void updateSupplier(int index, String supplierName, String supplierAddress, String supplierPhone, String supplierCountry, String supplierCategory) {
+        if (index >= 0 && index < suppliers.size()) {
+            if (supplierName != null) {
+                supplier.setSupplierName(supplierName);
+            }
+            if (supplierAddress != null) {
+                supplier.setSupplierAddress(supplierAddress);
+            }
+            if (supplierPhone != null) {
+                supplier.setSupplierPhone(supplierPhone);
+            }
+            if (supplierCountry != null) {
+                supplier.setSupplierCountry(supplierCountry);
+            }
+            if (supplierCategory != null) {
+                supplier.setSupplierCategory(supplierCategory);
+            }
+        } else {
+            System.out.println("Error");
+        }
+    }
+    public void removeSupplier(int index) {
+        if (index >= 0 && index < suppliers.size()) {
+            suppliers.remove(index);
+        }
+    }
+}
